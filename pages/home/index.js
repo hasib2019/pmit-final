4ded5399cd370dda071e79ffd09e4ad5c25687e6
hasib2019 <hasib.9437.hu@gmail.com>
2024-02-05 01:33:17 +0600

    update commit

---
+++ pages/home/index.js
@@ -0,0 +1,14 @@
+import { ConfigProvider } from 'antd'
+import React, { Fragment } from 'react'
+
+const index = () => {
+  return (
+   <Fragment>
+    <ConfigProvider>
+      Hasiob
+    </ConfigProvider>
+   </Fragment>
+  )
+}
+
+export default index
